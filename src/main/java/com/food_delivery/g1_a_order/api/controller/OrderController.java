a3921c9c8d1656d881879d314472e6c063a2a9fb
Ala-Alsanea <ala0alsanea@gmail.com>
2024-03-19 16:32:38 +0300

    `feat:` get orders

---
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.food_delivery.g1_a_order.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.food_delivery.g1_a_order.api.dto.OrderCreateDto;
+import com.food_delivery.g1_a_order.persistent.entity.Order;
+import com.food_delivery.g1_a_order.service.OrderService;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.data.domain.jaxb.SpringDataJaxb.OrderDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/order")
+public class OrderController {
+
+    private final OrderService OrderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders() {
+
+        return ResponseEntity.ok(OrderService.getOrders());
+    }
+}
