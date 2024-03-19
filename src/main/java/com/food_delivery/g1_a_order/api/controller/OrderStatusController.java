5e87cdfc2a7868f54a4f4f924001465dcdedbf4e
Ala-Alsanea <ala0alsanea@gmail.com>
2024-03-19 13:46:08 +0300

    `test` add controller to test dto

---
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderStatusController.java
@@ -0,0 +1,40 @@
+package com.food_delivery.g1_a_order.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.food_delivery.g1_a_order.api.dto.OrderStatusShowDto;
+import com.food_delivery.g1_a_order.config.MapperRegister;
+import com.food_delivery.g1_a_order.persistent.entity.OrderStatus;
+import com.food_delivery.g1_a_order.persistent.repository.OrderStatusRepository;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/orderStatus")
+public class OrderStatusController {
+
+    OrderStatusRepository repository;
+    @Autowired
+    MapperRegister mapper;
+
+    @GetMapping()
+    List<OrderStatusShowDto> getOrderStatus() {
+
+        var orderStatus = repository.findAll();
+        return mapper.toOrderStatusShowDto(orderStatus);
+    }
+
+    @GetMapping("/testString")
+    String test() {
+        List<OrderStatus> orderStatus = repository.findAll();
+        return orderStatus.get(0).toString();
+    }
+
+}
