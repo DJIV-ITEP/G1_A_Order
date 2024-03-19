dda27260a539d580ea70ff03cf5a4fb5392fad01
Ala-Alsanea <ala0alsanea@gmail.com>
2024-03-19 16:20:16 +0300

    `setup` order and order item dto

--- src/main/java/com/food_delivery/g1_a_order/config/MapperRegister.java
+++ src/main/java/com/food_delivery/g1_a_order/config/MapperRegister.java
@@ -3,14 +3,24 @@ package com.food_delivery.g1_a_order.config;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import com.food_delivery.g1_a_order.api.dto.OrderCreateDto;
 import com.food_delivery.g1_a_order.api.dto.OrderStatusShowDto;
+import com.food_delivery.g1_a_order.persistent.entity.Order;
 import com.food_delivery.g1_a_order.persistent.entity.OrderStatus;
 
 @Mapper(componentModel = "spring")
 public interface MapperRegister {
 
+    // order status
     OrderStatusShowDto toOrderStatusShowDto(OrderStatus orderStatus);
     List<OrderStatusShowDto> toOrderStatusShowDto(List<OrderStatus> orderStatus);
+    
+    // order
+    Order toOrder(OrderCreateDto orderCreateDto);
+    OrderCreateDto toOrderCreateDto(Order order);
+    List<OrderCreateDto> toOrderCreateDto(List<Order> order);
+    List<Order> toOrder(List<OrderCreateDto> orderCreateDto);
     
 }
