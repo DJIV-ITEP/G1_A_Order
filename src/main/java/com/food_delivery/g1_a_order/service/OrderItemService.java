482bee12c8160b99aed8ff1e8f5a3cfeaded3be5
Ala-Alsanea <ala0alsanea@gmail.com>
2024-03-22 15:59:49 +0300

    fix: delete empty order

--- src/main/java/com/food_delivery/g1_a_order/service/OrderItemService.java
+++ src/main/java/com/food_delivery/g1_a_order/service/OrderItemService.java
@@ -2,7 +2,10 @@ package com.food_delivery.g1_a_order.service;
 
 import org.springframework.stereotype.Service;
 
+import com.food_delivery.g1_a_order.persistent.entity.Order;
+import com.food_delivery.g1_a_order.persistent.entity.OrderItem;
 import com.food_delivery.g1_a_order.persistent.repository.OrderItemRepository;
+import com.food_delivery.g1_a_order.persistent.repository.OrderRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,12 +14,25 @@ import lombok.RequiredArgsConstructor;
 public class OrderItemService {
 
     private final OrderItemRepository itemRepository;
+    private final OrderRepository orderRepository;
 
     public boolean deleteOrderItem(Long id) {
 
         if (itemRepository.findById(id).isPresent()) {
+
+            OrderItem item = itemRepository.findById(id).get();
+
+            // System.out.println("________debug(size)_________");
+            // System.out.println(item.getOrder().getId());
+            // System.out.println("________debug_________");
+
+
             try {
                 itemRepository.deleteById(id);
+
+                if ( 0 == item.getOrder().getOrderItems().size()) {
+                    orderRepository.deleteById(item.getOrder().getId());
+                }
 
             } catch (IllegalArgumentException e) {
                 System.out.println(e);
