c4d59c59404170690602b1223aaebd96376331bc
Ala-Alsanea <ala0alsanea@gmail.com>
2024-03-24 20:53:41 +0300

    chore: set order update date

--- src/main/java/com/food_delivery/g1_a_order/service/OrderService.java
+++ src/main/java/com/food_delivery/g1_a_order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.food_delivery.g1_a_order.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -75,6 +76,7 @@ public class OrderService {
             StatusResponseHelper.notAcceptable("status is not acceptable");
 
         order.setOrderStatus(status);
+        order.setUpdatedAt(LocalDateTime.now());
         order = orderRepository.save(order);
 
         OrderShowDto dto = orderMapper
