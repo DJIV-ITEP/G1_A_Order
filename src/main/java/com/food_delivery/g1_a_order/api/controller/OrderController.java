1132c614e2a3b6b312aca1a2123452124d7c41cf
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2024-03-27 01:21:18 +0300

    chore(orderController): return `confirmOrder` endpoint

--- src/main/java/com/food_delivery/g1_a_order/api/controller/OrderController.java
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.food_delivery.g1_a_order.api.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.food_delivery.g1_a_order.api.dto.order.OrderCreateDto;
 import com.food_delivery.g1_a_order.api.dto.order.OrderShowDto;
 import com.food_delivery.g1_a_order.api.dto.orderItem.OrderItemsCreateDto;
 import com.food_delivery.g1_a_order.persistent.enum_.ResponseMsg;
@@ -30,7 +29,6 @@ public class OrderController {
     private final OrderService orderService;
     private final OrderItemService itemService;
 
-
     @GetMapping
     public ResponseEntity<List<OrderShowDto>> getOrders() {
 
@@ -40,20 +38,22 @@ public class OrderController {
 
     // may be wanted
     // @PostMapping("add")
-    // public ResponseEntity<String> addOrder(@RequestBody OrderCreateDto OrderCreateDto) {
+    // public ResponseEntity<String> addOrder(@RequestBody OrderCreateDto
+    // OrderCreateDto) {
 
-    //     if (orderService.createOrder(OrderCreateDto)) {
-    //         return ResponseEntity.ok(ResponseMsg.SUCCESS.message);
-    //     }
+    // if (orderService.createOrder(OrderCreateDto)) {
+    // return ResponseEntity.ok(ResponseMsg.SUCCESS.message);
+    // }
 
-    //     return ResponseEntity.ok(ResponseMsg.NOT_FOUND.message);
+    // return ResponseEntity.ok(ResponseMsg.NOT_FOUND.message);
     // }
 
     @PostMapping("/customer/{customerId}/restaurant/{restaurantId}/orderItem")
-    public ResponseEntity<String> addOrderItemToOrder(@PathVariable("customerId") Long customerId,@PathVariable("restaurantId") Long restaurantId,
+    public ResponseEntity<String> addOrderItemToOrder(@PathVariable("customerId") Long customerId,
+            @PathVariable("restaurantId") Long restaurantId,
             @RequestBody OrderItemsCreateDto itemDto) {
 
-        itemService.addOrderItemToOrder(customerId, restaurantId,itemDto);
+        itemService.addOrderItemToOrder(customerId, restaurantId, itemDto);
         return ResponseEntity.ok(ResponseMsg.SUCCESS.message);
     }
 
@@ -63,6 +63,17 @@ public class OrderController {
 
         return orderService.changeOrderStatus(orderId, orderStatusId);
 
+    }
+
+    @PostMapping("{orderId}/confirm")
+    public ResponseEntity<String> confirmOrder(@PathVariable("orderId") Long orderId) {
+
+        if (orderService.confirmOrder(orderId)) {
+            return ResponseEntity.ok(ResponseMsg.SUCCESS.message);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Failed to confirm order, please check if the order id is correct.");
     }
 
 }
