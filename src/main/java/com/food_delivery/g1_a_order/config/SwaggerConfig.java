7ddf805c7de53a756b34bc99fbbc60535165361e
Ala-Alsanea <ala0alsanea@gmail.com>
2024-03-27 15:26:35 +0300

    chore(swagger): remove auth

--- src/main/java/com/food_delivery/g1_a_order/config/SwaggerConfig.java
+++ src/main/java/com/food_delivery/g1_a_order/config/SwaggerConfig.java
@@ -13,32 +13,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
-        return new OpenAPI()
-                .info(new Info().title("Order Service")
-                        .description("This is order service use for managing order of food delivery application.")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
+                return new OpenAPI()
+                                .info(new Info().title("Order Service")
+                                                .description("This is order service use for managing order of food delivery application.")
+                                                .version("v1.0.0")
+                                                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
 
-                .externalDocs(new ExternalDocumentation()
-                        .description("SpringBoot Wiki Documentation")
-                        .url("https://springboot.wiki.github.org/docs"))
+                                .externalDocs(new ExternalDocumentation()
+                                                .description("SpringBoot Wiki Documentation")
+                                                .url("https://springboot.wiki.github.org/docs"))
 
-                .externalDocs(
-                        new ExternalDocumentation()
-                                .description("DJIV-ITEP camp training")
-                                .url("https://github.com/DJIV-ITEP/G1_A_Order"))
+                                .externalDocs(
+                                                new ExternalDocumentation()
+                                                                .description("DJIV-ITEP camp training")
+                                                                .url("https://github.com/DJIV-ITEP/G1_A_Order"))
 
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-                .components(
-                        new Components()
-                                .addSecuritySchemes(securitySchemeName,
-                                        new SecurityScheme()
-                                                .name(securitySchemeName)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")));
-    }
+                                // .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                // .components(
+                                //                 new Components()
+                                //                                 .addSecuritySchemes(securitySchemeName,
+                                //                                                 new SecurityScheme()
+                                //                                                                 .name(securitySchemeName)
+                                //                                                                 .type(SecurityScheme.Type.HTTP)
+                                //                                                                 .scheme("bearer")
+                                //                                                                 .bearerFormat("JWT")))
+                                                                                                ;
+        }
 }
