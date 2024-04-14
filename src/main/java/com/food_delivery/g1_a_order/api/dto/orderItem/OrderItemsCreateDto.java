21ab594ccb5a053f0e2523ee6bd8b436c5f516d4
Ala-Alsanea <ala0alsanea@gmail.com>
2024-04-14 19:02:24 +0300

    test: dto validation

--- src/main/java/com/food_delivery/g1_a_order/api/dto/orderItem/OrderItemsCreateDto.java
+++ src/main/java/com/food_delivery/g1_a_order/api/dto/orderItem/OrderItemsCreateDto.java
@@ -1,6 +1,8 @@
 package com.food_delivery.g1_a_order.api.dto.orderItem;
 
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public record OrderItemsCreateDto(
         Long itemId,
         Long quantity,
