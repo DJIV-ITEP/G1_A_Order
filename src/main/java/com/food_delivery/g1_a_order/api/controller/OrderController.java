919ca88746c58ca9ea46346d6a7356d42771f138
Ala-Alsanea <ala0alsanea@gmail.com>
2024-04-14 19:01:22 +0300

    ref: separate customer endpoints

--- src/main/java/com/food_delivery/g1_a_order/api/controller/OrderController.java
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderController.java
@@ -1,19 +1,15 @@
 package com.food_delivery.g1_a_order.api.controller;
 
-import com.food_delivery.g1_a_order.api.dto.order.OrderCreateDto;
 import com.food_delivery.g1_a_order.api.dto.order.OrderShowDto;
-import com.food_delivery.g1_a_order.persistent.enum_.OrderStatusEnum;
-import com.food_delivery.g1_a_order.persistent.enum_.ResponseMsg;
-import com.food_delivery.g1_a_order.service.OrderItemService;
+
 import com.food_delivery.g1_a_order.service.OrderService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class OrderController {
 
     private final OrderService orderService;
-    private final OrderItemService itemService;
 
     @GetMapping
     public ResponseEntity<List<OrderShowDto>> getOrders() {
@@ -30,29 +25,8 @@ public class OrderController {
 
     }
 
-    // may be wanted
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("add/orderItem")
-    public ResponseEntity<OrderShowDto> addOrderItems(@RequestBody OrderCreateDto orderCreateDto) {
 
-        return ResponseEntity.ok(itemService.addOrderItemToOrder(
-                orderCreateDto.customerId(),
-                orderCreateDto.restaurantId(),
-                orderCreateDto.orderItems()));
 
-    }
-
-    @PutMapping("{orderId}/customerConfirm")
-    public ResponseEntity<OrderShowDto> customerConfirmOrder(@PathVariable("orderId") Long orderId,
-            @RequestParam("addressId") Long addressId) {
-
-        return ResponseEntity.ok(orderService.customerChangeOrderStatus(
-                orderId,
-                addressId,
-                OrderStatusEnum.PENDING.status,
-                OrderStatusEnum.CART.status));
-
-    }
 
     @PutMapping("{orderId}/deliveryAccept")
     public ResponseEntity<OrderShowDto> deliveryAcceptOrder(@PathVariable("orderId") Long orderId,
@@ -68,10 +42,6 @@ public class OrderController {
         return ResponseEntity.ok(orderService.orderDelivered(orderId, deliveryId));
     }
 
-    @GetMapping("customer/{customerId}")
-    public ResponseEntity<List<OrderShowDto>> getOrdersByCustomer(@PathVariable("customerId") Long customerId) {
-        return ResponseEntity.ok(orderService.getOrdersByCustomer(customerId));
-    }
 
     @GetMapping("readyToPickup/delivery/{deliveryId}")
     public ResponseEntity<List<OrderShowDto>> getReadyToPickupOrdersByDelivery(
---
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderCustomerController.java
@@ -0,0 +1,65 @@
+package com.food_delivery.g1_a_order.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import com.food_delivery.g1_a_order.api.dto.order.OrderCreateDto;
+import com.food_delivery.g1_a_order.api.dto.order.OrderShowDto;
+import com.food_delivery.g1_a_order.persistent.enum_.OrderStatusEnum;
+import com.food_delivery.g1_a_order.service.OrderItemService;
+import com.food_delivery.g1_a_order.service.OrderService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/order")
+public class OrderCustomerController {
+
+    private final OrderService orderService;
+    private final OrderItemService itemService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("add/item")
+    public ResponseEntity<OrderShowDto> addOrderItems(@Valid @RequestBody OrderCreateDto orderCreateDto) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(itemService.addOrderItemToOrder(
+                        orderCreateDto.customerId(),
+                        orderCreateDto.restaurantId(),
+                        orderCreateDto.orderItems()));
+
+    }
+
+    @PutMapping("{orderId}/customerAddress/{addressId}/confirm")
+    public ResponseEntity<OrderShowDto> customerConfirmOrder(
+            @PathVariable("orderId") Long orderId,
+            @PathVariable("addressId") Long addressId) {
+
+        return ResponseEntity.ok(orderService.customerChangeOrderStatus(
+                orderId,
+                addressId,
+                OrderStatusEnum.PENDING.status,
+                OrderStatusEnum.CART.status));
+
+    }
+
+    @GetMapping("customer/{customerId}")
+    public ResponseEntity<List<OrderShowDto>> getOrdersByCustomer(@PathVariable("customerId") Long customerId) {
+        return ResponseEntity.ok(orderService.getOrdersByCustomer(customerId));
+    }
+
+}
