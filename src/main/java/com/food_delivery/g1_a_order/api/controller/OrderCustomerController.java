98d7a3a3a4d11d223999115a9843908347ba3b7a
Eng-Adam-Almohammedi <ealmohammedi@gmail.com>
2024-04-15 00:29:20 +0300

    feat: change confirm to setOrderAddress

--- src/main/java/com/food_delivery/g1_a_order/api/controller/OrderCustomerController.java
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderCustomerController.java
@@ -1,29 +1,17 @@
 package com.food_delivery.g1_a_order.api.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-
 import com.food_delivery.g1_a_order.api.dto.order.OrderCreateDto;
 import com.food_delivery.g1_a_order.api.dto.order.OrderShowDto;
 import com.food_delivery.g1_a_order.persistent.enum_.OrderStatusEnum;
 import com.food_delivery.g1_a_order.service.OrderItemService;
 import com.food_delivery.g1_a_order.service.OrderService;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -54,18 +42,14 @@ public class OrderCustomerController {
 
     }
 
-    @PutMapping("customer/confirm")
-    public ResponseEntity<OrderShowDto> customerConfirmOrder(@RequestBody customerConfirmDto body) {
-
+    @PutMapping("customer/setOrderAddress")
+    public ResponseEntity<OrderShowDto> customerSetOrderAddresses(@RequestBody customerConfirmDto body) {
         Long orderId = body.orderId();
         Long addressId = body.addressId();
 
-
-        return ResponseEntity.ok(orderService.customerChangeOrderStatus(
+        return ResponseEntity.ok(orderService.customerSetOrderAddresses(
                 orderId,
-                addressId,
-                OrderStatusEnum.PENDING.status,
-                OrderStatusEnum.CART.status));
+                addressId));
 
     }
 
