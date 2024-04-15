d658dbd9d3298c3231ad6dba06c4e4bd52bf92f2
Ala-Alsanea <ala0alsanea@gmail.com>
2024-04-15 20:09:22 +0300

    doc: add `ApiResponse` annotation to document the api for `OrderStatusController`

--- src/main/java/com/food_delivery/g1_a_order/api/controller/OrderStatusController.java
+++ src/main/java/com/food_delivery/g1_a_order/api/controller/OrderStatusController.java
@@ -13,26 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import com.food_delivery.g1_a_order.api.dto.response.ErrResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/orderStatus")
 public class OrderStatusController {
 
-
     private final OrderStatusService service;
 
     @Autowired
-    OrderStatusMapper  orderStatusMapper;
+    OrderStatusMapper orderStatusMapper;
 
     @GetMapping()
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(array = @ArraySchema(schema = @Schema(implementation = OrderStatusShowDto.class)))),
+})
     public ResponseEntity<List<OrderStatusShowDto>> getOrderStatus() {
 
-
         List<OrderStatusShowDto> orderStatus = service.getOrderStatus();
-        
+
         return ResponseEntity.ok(orderStatus);
     }
-
-
 
 }
