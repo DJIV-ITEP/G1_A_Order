5433b84871d51f7223f9e919f04f3e2036e7a3f5
Ala-Alsanea <ala0alsanea@gmail.com>
2024-04-18 22:35:26 +0300

    ref: create `CustomerService`

---
+++ src/main/java/com/food_delivery/g1_a_order/service/CustomerService.java
@@ -0,0 +1,23 @@
+package com.food_delivery.g1_a_order.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    @Autowired
+    private final WebClient webClient;
+
+    // todo: handle fetching email from customer service
+    public String getEmail(Long customerId) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'getEmail'");
+
+    }
+
+}
