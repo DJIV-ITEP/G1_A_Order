9436a8c2288717c8407e88f6a38f99e428728e03
Amjad Fouad Al-Aghbari <amjadffqs@gmail.com>
2024-04-18 12:15:44 +0300

    feat(release-version): update version automaticlly

--- src/main/java/com/food_delivery/g1_a_order/config/SwaggerConfig.java
+++ src/main/java/com/food_delivery/g1_a_order/config/SwaggerConfig.java
@@ -4,11 +4,16 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+        @Value("${info.app.version}")
+        private String appVersion;
 
         @Bean
         public OpenAPI customOpenAPI() {
@@ -16,7 +21,7 @@ public class SwaggerConfig {
                 return new OpenAPI()
                                 .info(new Info().title("Order Service")
                                                 .description("This is order service use for managing order of food delivery application.")
-                                                .version("v1.5.0")
+                                                .version(appVersion)
                                                 .license(new License().name("Apache 2.0").url("http://springdoc.org")))
 
                                 .externalDocs(new ExternalDocumentation()
